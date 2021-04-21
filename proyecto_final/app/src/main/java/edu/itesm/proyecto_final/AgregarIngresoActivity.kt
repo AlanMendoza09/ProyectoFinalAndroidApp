804373dd8d84e662700f7308baa5da756d8163c7
NavarroDGerardo <dgnvrrg@gmail.com>
2021-04-21 17:16:41 -0500

    pantallas de ingreso y ahorro

---
+++ proyecto_final/app/src/main/java/edu/itesm/proyecto_final/AgregarIngresoActivity.kt
@@ -0,0 +1,11 @@
+package edu.itesm.proyecto_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AgregarIngresoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agregar_ingreso)
+    }
+}
